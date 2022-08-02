ed1fcd944cd47d306311a1c977e7ef6dbf2b3896
ph <ph@flashapp.com.br>
2022-08-02 20:04:31 -0300

    session

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const express = require('express')
 
 const baseUrl = 'https://www.balldontlie.io/api/v1'
 
-async function getLeBron(request, response) {
+/**
+ * Function designed to search a player using Ball Don't Lie API
+ * 
+ * @param {import('axios').AxiosRequestConfig} req 
+ * @param {import('axios').AxiosResponse} res 
+ * @returns {import('axios').AxiosResponse} player data
+ */
+async function searchPlayer(req, res) {
   try {
-    const result = await axios.get(`${baseUrl}/players?search=lebron`)
+    if (!req.body || !req.body.playerName) {
+      return res.status(400).send('Missing param')
+    }
+
+    const response = await axios.get(`${baseUrl}/players?search=${req.body.playerName}`)
     // o tipo de uma "response" é muito muito grande. evite dar console.log nele inteiro
     // o resultado vem dentro da chave "data"
   
     // coincidentemente, a API da Ball Don't Lie retorna outra chave data pros resultados, q é um array
-    const lebron = result.data.data[0]
-    return response.status(200).send(JSON.stringify(lebron, null, 2))
+    if (!response.data.data.length) {
+      return res.status(400).send('No players found');
+    }
+    const lebron = response.data.data
+    return res.status(200).send(JSON.stringify(lebron, null, 2))
   } catch (error) {
     console.error(error);
     // faço isso pra pegar se quem deu erro foi a chamada do axios
@@ -19,15 +33,16 @@ async function getLeBron(request, response) {
     // vamos querer retornar uma msg tratada
     // outra abordagem é sempre retornar uma msg genérica quando algo der errado no back
     if (error.response) {
-      return response.status(error.response.status).send(error.message)
+      return res.status(error.response.status).send(error.message)
     }
-    return response.status(500).send(error.message)
+    return res.status(500).send(error.message)
   }
 }
 
 const app = express()
 
 // workaround pra sempre já ter os cabeçalho treta
+app.use(express.json())
 app.use((_request, response, next) => {
   response.append('Access-Control-Allow-Origin', ['*'])
   response.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
@@ -47,14 +62,21 @@ app.use((_request, response, next) => {
   next();
 });
 
-app.get('/lebron', async (request, response) => {
-  console.log('fetching lebron...')
+app.post('/player/search', async (request, response) => {
+  console.log('fetching player...')
   // eu acabei passando pra frente a _response_ pra poder responder por dentro da função
   // _getLeBron_, mas vc poderia fazer a função _getLeBron_ só retornar os dados da API
   // e aqui dentro da definição da função que lida com GET em '/lebron' decidir se manda resposta,
   // com qual status e etc
-  await getLeBron(request, response)
-  console.log('lebron delivered!')
+  await searchPlayer(request, response)
+  console.log('player delivered!')
+})
+
+app.get('/teste', async (request, response) => {
+  console.log('nova rota')
+
+  const apiResponse = await axios.get(`${baseUrl}/season_averages?season=2018&player_ids[]=237`)
+  return response.status(200).send(JSON.stringify(apiResponse, null, 2))
 })
 
 app.listen('9999', () => {
